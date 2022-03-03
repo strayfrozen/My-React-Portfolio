8ec2b14b8c20127595cd054bfef5281fbe8b12ea
Stray <strayfrozen@yahoo.com>
2022-03-03 15:41:26 -0600

    added css to Header and Navigation componets stylized Navigation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navigation from '../Navigation';
 
+import './Header.css';
+
 function Header() {
   return (
     <header>
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Navigation.css'
 
 function Navigation() {
 
   return (
-    <nav>
-        <h2>About Me</h2>
-        <h2>Portfolio</h2>
-        <h2>Contact</h2>
-        <h2>Resume</h2>
+    <nav className="navbar">
+        <h2 className='nav-link'>About Me</h2>
+        <h2 className='nav-link'>Portfolio</h2>
+        <h2 className='nav-link'>Contact</h2>
+        <h2 className='nav-link'>Resume</h2>
     </nav>
   );
 }
