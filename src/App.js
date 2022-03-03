05ef52c1e010593b3a90a46706657b8fe526afff
Stray <strayfrozen@yahoo.com>
2022-03-03 14:00:01 -0600

    imported header into app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header';
 function App() {
+
   return (
     <div>
-      <p>Hello World</p>
+      <main>
+        <Header></Header>
+      </main>
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <section>
+      <h1 id="Header">Chris Langford</h1>
+    </section>
+  );
+}
+
+export default Header;
