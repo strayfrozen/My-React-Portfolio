b78831d9b76a4340b6f7234a3383c70b5d6e6f1b
Stray <strayfrozen@yahoo.com>
2022-03-03 14:18:30 -0600

    set up basic nav component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Navigation from './components/Navigation';
 function App() {
 
   return (
     <div>
       <main>
         <Header></Header>
+        <Navigation></Navigation>
       </main>
     </div>
   );
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Nav() {
+
+  return (
+    <nav>
+        <h2>About Me</h2>
+        <h2>Portfolio</h2>
+        <h2>Contact</h2>
+        <h2>Resume</h2>
+    </nav>
+  );
+}
+
+export default Nav;
