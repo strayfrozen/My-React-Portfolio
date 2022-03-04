5a4a06975f24ac3ae9008c9ba21bc0ba60b37bd4
Stray <strayfrozen@yahoo.com>
2022-03-04 13:29:14 -0600

    created AboutMe component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
+import AboutMe from './components/AboutMe';
 function App() {
 
   return (
     <div>
       <main>
         <Header />
+        <AboutMe />
       </main>
     </div>
   );
---
+++ src/components/AboutMe/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './AboutMe.css';
+
+
+
+
+function AboutMe() {
+  return (
+    <section>
+        <h1>About Me</h1>
+        <p>My name is Chris Langford and I am a web developer.I am currently attending 
+            Vanderbilt's fullstack javascript boot camp. I have learned many cutting edge technologies and skills
+            during the program. I'm currently looking for employment in any development related fields.
+        </p>
+    </section>
+  );
+}
+
+export default AboutMe;
