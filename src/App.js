5e91aa32b0fc7fe1e5f0c51daf9dd94d61f06069
Stray <strayfrozen@yahoo.com>
2022-03-06 19:58:28 -0600

    refactored webpage

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import Footer from './components/Footer';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
 import Project from './components/Project';
-import {BrowserRouter} from 'react-router-dom'
 
-import{Switch,Route, Redirect, withRouter} from 'react-router-dom'
+
+
+
+
 
 
 
@@ -18,20 +20,15 @@ function App() {
   return (
     <div>
       <main>
-        <BrowserRouter>
         <Header />
-        
-        <Switch>
-        <Route path="/about" component={AboutMe} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/project" component={Project} />
-        <Route path="/resume" component={Resume} />
-        </Switch>
+        <AboutMe />
+        <Contact />
+        <Project />
+        <Resume />
         <Footer />
-        </BrowserRouter>
       </main>
     </div>
   );
 }
 
-export default withRouter(App);
+export default App;
